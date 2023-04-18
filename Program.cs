b5f1305006d7963d3bc94ff635417d9d63ae2c51
Silent <silent3_3@mail.ru>
2023-04-18 17:44:05 +0300

    Добавлен список веток на main

--- Program.cs
+++ Program.cs
@@ -1,44 +1,12 @@
-﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿
 
-m = 3, n = 4.
 
-0,5 7 -2 -0,2
-
-1 -3,3 8 -9,9
-
-8 7,8 -7,1 9 */
-
-double[,] array = Generate2DArray(3,4);
-PrintArray(array);
-
-void PrintArray(double[,] array)
-{
-    for (var i = 0; i < array.GetLength(0); i++)
-    {
-        for (var j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"<{array[i,j]}>");
-            Console.Write(" ");
-        }
-
-        Console.WriteLine();
-    }
-}
-
-double[,] Generate2DArray(int m, int n)
-{
-    double[,] array = new double[m, n];
-
-    Random random = new Random();
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            array[i, j] = random.Next(-100, 100);
-            array[i, j] /= 10;
-        }
-    }
-
-    return array;
-}
+/*
+    Задания находятся в соответствующих ветках.
+    
+    Список веток:
+    main
+    task47
+    task50
+    task52
+*/
